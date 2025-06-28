5d6ba1a1af7e58d76c55ae1d93aa30c21a1aa2f1
gustavoking <gustavorostirollax@gmail.com>
2025-06-28 16:37:19 -0300

    feat: add badge

--- app/page.tsx
+++ app/page.tsx
@@ -3,35 +3,37 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <div className="px-4 py-6 md:px-8 lg:px-20 xl:px-40">
-        <h2 className="text-xl font-bold md:text-2xl">Olá, Gustavo</h2>
-        <p className="text-sm md:text-base">Segunda-feira, 28 de junho</p>
-
-        {/* Área da busca e banner */}
-        <div className="mt-6 flex flex-col items-stretch gap-4 lg:flex-row">
-          {/* Input + Botão */}
-          <div className="flex flex-1 flex-row gap-2">
-            <Input placeholder="Buscar..." className="flex-1" />
-            <Button className="w-12 lg:w-14">
-              <SearchIcon className="h-5 w-5" />
-            </Button>
-          </div>
-
-          {/* Banner */}
-          <div className="relative h-[150px] lg:h-auto lg:w-[250px]">
-            <Image
-              alt="Banner"
-              src="/banner-01.png"
-              fill
-              className="rounded-xl object-cover"
-            />
-          </div>
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gustavo</h2>
+        <p>Segunda-feira, 28 de junho</p>
+        <div className="mt-6 flex flex-row justify-between gap-2 p-5">
+          <Input placeholder="Buscar..." />
+          <Button>
+            <SearchIcon />
+          </Button>
         </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende FSWBARBER"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+        <Card className="mt-6">
+          <CardContent className="flex">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge>Confirmado</Badge>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
