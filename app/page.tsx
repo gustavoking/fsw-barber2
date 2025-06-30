1809ec988ba8591dcaecee35ad4b75edd28512ab
gustavoking <gustavorostirollax@gmail.com>
2025-06-30 10:20:54 -0300

    feat: add booking item and avatar

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -13,12 +14,14 @@ export default function Home() {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Gustavo</h2>
         <p>Segunda-feira, 28 de junho</p>
-        <div className="mt-6 flex flex-row justify-between gap-2 p-5">
+
+        <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Buscar..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
+
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende FSWBARBER"
@@ -28,9 +31,21 @@ export default function Home() {
           />
         </div>
         <Card className="mt-6">
-          <CardContent className="flex">
+          <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge>Confirmado</Badge>
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3>Corte de cabelo</h3>
+              <div className="flex items-center">
+                <Avatar>
+                  <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
             </div>
           </CardContent>
         </Card>
