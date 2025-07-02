7533f10da339f58cfcc0b8385f4bf4b6fc51c7ef
gustavoking <gustavorostirollax@gmail.com>
2025-07-02 11:16:37 -0300

    feat: add SidebarSheet component

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
+import SidebarSheet from "@/app/_components/sidebar-sheet"
 import { Button } from "@/app/_components/ui/button"
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet"
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
@@ -48,14 +50,19 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
             <ChevronLeftIcon />
           </Link>
         </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="secondary"
+              className="absolute right-4 top-4"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
 
-        <Button
-          size="icon"
-          variant="outline"
-          className="absolute right-4 top-4"
-        >
-          <MenuIcon />
-        </Button>
+          <SidebarSheet />
+        </Sheet>
       </div>
 
       {/* TÍTULO */}
